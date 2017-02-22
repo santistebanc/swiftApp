745e03a38fcfd5bad94d4ca96ddb5f0f8bb1d1c4
XCODE <xcode@iMac-de-Administrador-7.local>
2017-02-21 19:20:42 -0600

    Initial Commit

---
+++ app_supermegachida/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app_supermegachida
+//
+//  Created by XCODE on 21/02/17.
+//  Copyright (c) 2017 XCODE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
